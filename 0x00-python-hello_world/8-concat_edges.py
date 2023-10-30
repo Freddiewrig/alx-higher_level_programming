4d1726e23a02fee80d3b64344c22c7ec0cbfe6c4
Freddiewrig <thecollegeace1@gmail.com>
2023-10-30 11:39:07 -0700

    hello world

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 # Use slicing to print the desired output
-print(str[39:66] + str[106:117] + str[0:6])
+print(str[39:66] + str[106:112] + str[:6])
