1b5a9a5f6493ca1aa726ef60bf469617ebd63464
Freddiewrig <thecollegeace1@gmail.com>
2023-11-24 03:09:49 -0800

    classes

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""define class node"""
+
+
+class Node:
+    """init the class node"""
+
+    def __init__(self, data, next_node=None):
+        """initialize new node.
+
+        args:
+            data: must be an int
+            next_node: node.
+        """
+        self.data = data
+        self.next_node = next_node
+    @property
+    def data(self):
+        """get/set data"""
+        return (self.__data)
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+    @property
+    def next_node(self):
+        """get/set next_node"""
+        return (self.__next_node)
+    @next_node.setter
+    def next_node(self, value):
+        if not (isinstance(value, Node)) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+class SinglyLinkedList:
+    """def linkedlist"""
+    def __init__(self):
+        self.__head = None
+    """def sorted_insert"""
+    def sorted_insert(self, value):
+        """insert new node to liskedlist.
+
+        the node is inserted at the correct
+        numerical order
+        args:
+            value(node): new node.
+        """
+        new_node = Node(value)
+        if self.__head is None:
+            new_node.next_node = None
+            self.__head = new_node
+        elif self.__head.data > value:
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            tmp = self.__head
+            while (tmp.next_node is not None and
+                    tmp.next_node.data < value):
+                tmp = tmp.next_node
+            new_node.next_node = tmp.next_node
+            tmp.next_node = new_node
+    def __str__(self):
+        result = []
+        tmp = self.__head
+        while tmp is not None:
+            result.append(str(tmp.data))
+            tmp = tmp.next_node
+        return ('\n'.join(result))
