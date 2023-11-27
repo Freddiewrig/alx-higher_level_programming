561b689fe171d64234c9f7436f61e4ad3c6fbd98
Freddiewrig <thecollegeace1@gmail.com>
2023-11-27 03:19:03 -0800

    more_classes

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -17,27 +17,27 @@ class Rectangle:
 
     @property
     def width(self):
-        """Get/set the width of the rectangle."""
-        return self__width
+        """Get/set the width of the Rectangle."""
+        return self.__width
 
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """Get/set the height of the rectangle."""
-        return self__height
+        """Get/set the height of the Rectangle."""
+        return self.__height
 
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
@@ -48,6 +48,5 @@ class Rectangle:
     def perimeter(self):
         """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
-            return 0
-        else:
-            return ((self.__width *2) + (self.__height * 2))
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
